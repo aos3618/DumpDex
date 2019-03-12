5c96e0a017182ce9f7a6fba83fc9123533044c40
AoS <liuzhanqi@dpandora.cn>
2019-03-12 15:48:10 +0800

    初始提交

---
+++ dexdumper/src/main/java/com/xposed/Dumper.java
@@ -0,0 +1,68 @@
+package com.xposed;
+
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+/**
+ * Created by AoS on 2017/10/24.
+ */
+
+public class Dumper implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+
+        Class clazz = loadPackageParam.classLoader.loadClass("com.android.dex.Dex");
+        if (clazz != null) {
+            XposedHelpers.findAndHookMethod(clazz, "create", ByteBuffer.class, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    Log.d("AoS ******", "beforeHookedMethod param is : " + loadPackageParam.processName + " / " + loadPackageParam.packageName);
+
+                    if ("your.package.name".equals(loadPackageParam.processName)) {
+                        dump((ByteBuffer) param.args[0], loadPackageParam.packageName);
+                    }
+                    super.beforeHookedMethod(param);
+                }
+
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    Log.d("AoS ******", "afterHookedMethod param is : " + param);
+                }
+            });
+        }
+    }
+
+    private static int num = 1;
+
+    private static void dump(ByteBuffer d, String processName) {
+        d.order(ByteOrder.LITTLE_ENDIAN);
+        d.duplicate();
+        byte[] bytes = new byte[d.remaining()];
+        d.get(bytes);
+        String path = Environment.getExternalStorageDirectory().getPath() + "/dump" + "/dump_" + processName + num + ".dex";
+        Log.e("AoS ******", "Begin dump dex :" + path + "  size :" + bytes.length);
+        writeByteToFile(bytes, path);
+        Log.e("AoS ******", "Finished dump dex :" + path + "  size :" + bytes.length);
+        num++;
+
+    }
+
+    private static void writeByteToFile(byte[] data, String path) {
+        try {
+            FileOutputStream localFileOutputStream = new FileOutputStream(path);
+            localFileOutputStream.write(data);
+            localFileOutputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
